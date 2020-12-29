8f4237a25dc494506966e0d577255c721ce2c237
Gabriel Peredo <gabriel.peredo777@gmail.com>
2020-12-29 12:54:09 -0400

    start: Inicio del proyecto

---
+++ src/index.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const morgan = require('morgan');
+
+
+// Settings
+app.set('port', process.env.PORT || 3000);
+app.set('json spaces', 2);
+
+
+// Middlewares
+app.use(morgan('common'));
+app.use(express.urlencoded({extended: false}));
+app.use(express.json());
+
+
+// Starting server
+app.use('/api/productos', require('./routes/productos'));
+app.use('/api/usuarios', require('./routes/usuarios'));
+app.listen(app.get('port'), () => {
+    console.log(`Server on port ${app.get('port')}`);
+})
